3048382fd6913148f33127951fc48f6326d5ce1f
Chris Wright <chris@inkyspider.co.uk>
2020-10-20 23:24:10 +0100

    Remove minified code

--- util/pid.lua
+++ util/pid.lua
@@ -30,7 +30,3 @@ function PID:Create()
 	end
 	return this
 end
-
--- minified
-
-PID={}function PID:Create()local a={interval=1/60,kP=0,kI=0,kD=0,min=0,max=1,clamp=false,_previous_err=0,_integral=0}function a:process(b,c)err=b-c;p_out=self.kP*err;self._integral=self._integral+err*self.interval;i_out=self.kI*self._integral;d_out=self.kD*(err-self._previous_err)/self.interval;result=p_out+i_out+d_out;if self.clamp and result>self.max then result=self.max elseif self.clamp and result<self.min then result=self.min end;self._previous_err=err;return result end;return a end
--- util/variable.lua
+++ util/variable.lua
@@ -33,7 +33,3 @@ function Variable:Create()
 
 	return this
 end
-
--- minified
-
-Variable={}function Variable:Create()local a={previous=0,current=0,incrementing=false,decrementing=false}function a:set(b)self.previous=self.current;self.current=b;if b>self.previous then self.incrementing=true;self.decrementing=false elseif b<self.previous then self.incrementing=false;self.decrementing=true end end;function a:is_incrementing()return self.incrementing and not self.decrementing end;function a:is_decrementing()return self.decrementing and not self.incrementing end;function a:offset(b)self:set(self.current+b)end;return a end
