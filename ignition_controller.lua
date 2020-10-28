d2e96c1a720f45225acdcda577731acac75316f5
Chris Wright <chris@inkyspider.co.uk>
2020-10-28 20:18:22 +0000

    Prevent ignition when thermal protection has been triggered

--- ignition_controller.lua
+++ ignition_controller.lua
@@ -5,9 +5,10 @@ ignition_rps = property.getNumber("Ignition RPS")
 
 function onTick()
 	local enabled = input.getBool(1)
+	local thermal_protect = input.getBool(16)
 	engine_rps:set(input.getNumber(1))
 
-	if enabled and engine_rps.current < ignition_rps then
+	if not thermal_protect and enabled and engine_rps.current < ignition_rps then
 		output.setBool(1, true)
 	else
 		output.setBool(1, false)
