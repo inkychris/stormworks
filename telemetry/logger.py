679c9d56b584c7cec3045d3025523a695e16f5fb
Chris Wright <chris@inkyspider.co.uk>
2020-10-31 18:20:49 +0000

    Change logging API to take packetised values

--- telemetry/logger.py
+++ telemetry/logger.py
@@ -3,13 +3,11 @@ import datetime
 import fastapi
 import pathlib
 import threading
-import typing
 
 
 class Logger:
     def __init__(self, *, dataset_timeout, output_dir=None):
         self._data = []
-        self._last_index = None
         self._output_dir = pathlib.Path(output_dir or 'data') / datetime.datetime.now().isoformat().replace(':', '-').replace('.', '-')
         self._dataset_timeout = datetime.timedelta(seconds=dataset_timeout)
         self._last_logged = None
@@ -25,11 +23,10 @@ class Logger:
 
     def reset(self):
         self._data = []
-        self._last_index = None
 
-    def log(self, index, value):
-        self._data.append((index, value))
-        self._last_index = index
+    def log(self, packet):
+        for entry in packet.split('|'):
+            self._data.append(entry.split(':'))
         self._last_logged = datetime.datetime.now()
 
     def dump(self):
@@ -61,13 +58,13 @@ class Logger:
             self._dump_thread.join()
 
 
-logger = Logger(dataset_timeout=3)
+logger = Logger(dataset_timeout=5)
 logger.start_auto_dump()
 app = fastapi.FastAPI()
 
 @app.get('/log')
-def log(index: int, value: typing.Any):
-    logger.log(index, value)
+def log(packet: str):
+    logger.log(packet)
     return "OK!"
 
 @app.get('/dump')
