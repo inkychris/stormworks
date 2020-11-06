23d1fffd64438328ac7898f8a8c6317a8de56ae3
Chris Wright <chris@inkyspider.co.uk>
2020-11-06 18:15:08 +0000

    Condense clamp function

--- util/clamp.lua
+++ util/clamp.lua
@@ -1,8 +1,5 @@
 function clamp(value, min, max)
-	if value > max then
-		value = max
-	elseif value < min then
-		value = min
-	end
+	if value > max then value = max
+	elseif value < min then value = min end
 	return value
 end
