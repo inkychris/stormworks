7c57878e37589c9004d8b340f48693a2c19eef5a
Chris Wright <chris@inkyspider.co.uk>
2020-11-06 18:14:48 +0000

    Use target RPS PID control for throttle controller

--- throttle_controller.lua
+++ throttle_controller.lua
@@ -1,89 +1,66 @@
 -- PID
 -- Variable
+-- Logger
 
-idler_pid = PID:Create()
-idler_pid.kP = property.getNumber("Idler PID (P)")
-idler_pid.kI = property.getNumber("Idler PID (I)")
-idler_pid.kD = property.getNumber("Idler PID (D)")
+logger = Logger:Create()
+should_log = property.getBool("Log Engine RPS")
 
-limiter_pid = PID:Create()
-limiter_pid.kP = property.getNumber("Limiter PID (P)")
-limiter_pid.kI = property.getNumber("Limiter PID (I)")
-limiter_pid.kD = property.getNumber("Limiter PID (D)")
-limiter_pid.clamp_integral = true
+pid = PID:Create{
+	kP = property.getNumber("Throttle PID (P)"),
+	kI = property.getNumber("Throttle PID (I)"),
+	kD = property.getNumber("Throttle PID (D)")
+}
 
-target_rate = Variable:Create()
-engine_rps = Variable:Create()
 min_rps = property.getNumber("Min Engine RPS")
 max_rps = property.getNumber("Max Engine RPS")
 max_reverse_rps = property.getNumber("Max Reverse Engine RPS")
 current_max_rps = max_rps
 max_engine_temp = property.getNumber("Max Engine Temp")
 
-idler_enabled = false
-limiter_enabled = false
-limiter_enable_target_rate = 0
-limiter_antirepeat = nil
 thermal_protect = false
 
-engine_rps_channel = property.getNumber("Engine RPS Channel (Number)")
-target_rate_channel = property.getNumber("Target Rate Channel (Number)")
-throttle_channel = property.getNumber("Throttle Channel (Number)")
-engine_temp_channel = property.getNumber("Engine Temp Channel (Number)")
-
-enabled_channel = property.getNumber("Enabled Channel (Bool)")
-reverse_channel = property.getNumber("Reverse Channel (Bool)")
-therm_reset_channel = property.getNumber("Thermal Reset Channel (Bool)")
-therm_protect_channel = property.getNumber("Thermal Protect Enabled Channel (Bool)")
+channel = {
+	engine_rps = 1,
+	target_rate = 2,
+	throttle = 3,
+	engine_temp = 5,
+	target_rps = 8,
+	--
+	enabled = 1,
+	reverse = 3,
+	thermal_reset = 4,
+	thermal_protect = 5,
+}
 
 function onTick()
-	engine_rps:set(input.getNumber(engine_rps_channel))
-	target_rate:set(input.getNumber(target_rate_channel))
+	for i=1,32 do output.setNumber(i,input.getNumber(i)); output.setBool(i,input.getBool(i)) end
+	logger:tick()
 
-	local throttle = target_rate.current
-	if limiter_antirepeat then
-		limiter_antirepeat = limiter_antirepeat + 1
-		if limiter_antirepeat > 20 then
-			limiter_antirepeat = nil
-		end
-	end
-	if input.getBool(enabled_channel) then
-		if engine_rps.current < min_rps then
-			idler_enabled = true
-		elseif not limiter_antirepeat and (engine_rps.current > current_max_rps) then
-			limiter_enabled = true
-			limiter_antirepeat = 0
-			limiter_enable_target_rate = target_rate.current
-		end
+	local engine_rps = input.getNumber(channel.engine_rps)
+	local target_rate = input.getNumber(channel.target_rate)
+
+	local target_rps = min_rps + (max_rps - min_rps) * target_rate
+	local throttle = 0
+	if input.getBool(channel.enabled) then
+		throttle = pid:process(target_rps, engine_rps)
+		if should_log then logger:log(engine_rps) end
 	else
-		idler_enabled = false
-		limiter_enabled = false
-		throttle = 0
+		logger:reset()
 	end
-	if idler_enabled then
-		throttle = idler_pid:process(min_rps, engine_rps.current)
-		if target_rate.current > throttle then
-			idler_enabled = false
-		end
-	end
-	if limiter_enabled then
-		throttle = limiter_pid:process(current_max_rps, engine_rps.current)
-		if target_rate.current < limiter_enable_target_rate then
-			limiter_enabled = false
-		end
-	end
-	if thermal_protect or input.getNumber(engine_temp_channel) > max_engine_temp then
+
+	if thermal_protect or input.getNumber(channel.engine_temp) > max_engine_temp then
 		throttle = 0
 		thermal_protect = true
 	end
-	if input.getBool(therm_reset_channel) then
+	if input.getBool(channel.thermal_reset) then
 		thermal_protect = false
 	end
-	if input.getBool(reverse_channel) then
+	if input.getBool(channel.reverse) then
 		current_max_rps = max_reverse_rps
 	else
 		current_max_rps = max_rps
 	end
-	output.setNumber(throttle_channel, throttle)
-	output.setBool(therm_protect_channel, thermal_protect)
+	output.setNumber(channel.throttle, throttle)
+	output.setNumber(channel.target_rps, target_rps)
+	output.setBool(channel.thermal_protect, thermal_protect)
 end
