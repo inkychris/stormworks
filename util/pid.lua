cfce93c9809709c613bc57ed74f03fa91e6077a5
Chris Wright <chris@inkyspider.co.uk>
2020-11-06 18:10:25 +0000

    Use integral anti-windup method that doesn't require clamp values

--- util/pid.lua
+++ util/pid.lua
@@ -1,39 +1,30 @@
--- source https://gist.github.com/bradley219/5373998
-
--- clamp
-
 PID = {}
-function PID:Create()
+function PID:Create(t)
 	local this = {
-		kP = 0,
-		kI = 0,
-		kD = 0,
-		clamp_output = false,
-		min = 0,
-		max = 1,
-		clamp_integral = false,
-		integral_min = -1,
-		integral_max = 1,
+		kP = t.kP or 1,
+		kI = t.kI or 0,
+		kD = t.kD or 0,
+		min = t.min or 0,
+		max = t.max or 1,
 		_previous_err = 0,
 		_integral = 0
 	}
-
 	function this:process(setpoint, pv)
 		local err = setpoint - pv
 		local p_out = self.kP * err
-		self._integral = self._integral + err / 60
-		if self.clamp_integral then
-			self._integral = clamp(self._integral, self.integral_min, self.integral_max)
-		end
+		self._integral = self._integral + (err / 60)
 		local i_out = self.kI * self._integral
-		local d_out = self.kD * ((err - self._previous_err) * 60)
-		local result = p_out + i_out + d_out
-		if self.clamp_output then
-			result = clamp(result, self.min, self.max)
+		local d_out = self.kD * (err - self._previous_err) * 60
+		local result = p_out + i_out - d_out
+		if result > self.max then
+			self._integral = self._integral - (err / 60)
+			result = self.max
+		elseif result < self.min then
+			self._integral = self._integral - (err / 60)
+			result = self.min
 		end
 		self._previous_err = err
 		return result
 	end
-
 	return this
 end
