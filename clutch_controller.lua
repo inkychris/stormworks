513227cc31e8d243eb30a27c7ce4854d6878fe6c
Chris Wright <chris@inkyspider.co.uk>
2020-11-06 18:15:46 +0000

    Simplify clutch controller

--- clutch_controller.lua
+++ clutch_controller.lua
@@ -1,28 +1,23 @@
--- Variable
 -- PID
 
 clutch_rps = property.getNumber("Clutch RPS")
 
-clutch_pid = PID:Create()
-clutch_pid.clamp_output = true
-clutch_pid.min = -1
-clutch_pid.max = 0
-clutch_pid.clamp_integral = true
-clutch_pid.kP = property.getNumber("Clutch PID (P)")
-clutch_pid.kI = property.getNumber("Clutch PID (I)")
-clutch_pid.kD = property.getNumber("Clutch PID (D)")
-
-engine_rps = Variable:Create()
-target_rate = Variable:Create()
+clutch_pid = PID:Create{
+	kP = property.getNumber("Clutch PID (P)"),
+	kI = property.getNumber("Clutch PID (I)"),
+	kD = property.getNumber("Clutch PID (D)")
+}
 
 function onTick()
-	local enabled = input.getBool(1)
-	engine_rps:set(input.getNumber(1))
-	target_rate:set(input.getNumber(2))
+	for i=1,32 do output.setNumber(i,input.getNumber(i)); output.setBool(i,input.getBool(i)) end
 
-	if not enabled or target_rate.current == 0 then
-		output.setNumber(1, 0)
+	local enabled = input.getBool(1)
+	local engine_rps = input.getNumber(1)
+	local target_rate = input.getNumber(2)
+
+	if (not enabled) or (target_rate == 0) then
+		output.setNumber(4, 0)
 	else
-		output.setNumber(1, -clutch_pid:process(clutch_rps, engine_rps.current))
+		output.setNumber(4, clutch_pid:process(engine_rps, clutch_rps))
 	end
 end
