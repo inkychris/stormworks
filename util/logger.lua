6d7ad39f1ff04241756670239e7305abac27f6fa
Chris Wright <chris@inkyspider.co.uk>
2020-11-27 12:28:43 +0000

    Simplify logger variable names and add table constructor for port and packet size

--- util/logger.lua
+++ util/logger.lua
@@ -1,28 +1,27 @@
-Logger = {}
-function Logger:Create()
+function Logger(t)
 	local this = {
-		_tick = 0,
-		port = 8000,
-		packet_length = 16,
-		_data = {},
+		tick = 0,
+		port = t.port or 8000,
+		packet_size = t.packet_size or 16,
+		data = {},
 	}
 	function this:tick()
-		self._tick = self._tick + 1
+		self.tick = self.tick + 1
 	end
 	function this:log(value)
-		self._data[#self._data + 1] = string.format("%s:%s", tostring(self._tick), tostring(value))
-		if #self._data > self.packet_length then
+		self.data[#self.data + 1] = string.format("%s:%s", tostring(self.tick), tostring(value))
+		if #self.data > self.packet_size then
 			self:send()
 		end
 	end
 	function this:reset()
 		self:send()
-		if #self._data > 0 then self._data = {} end
+		if #self.data > 0 then self.data = {} end
 	end
 	function this:send()
-		if #self._data == 0 then return end
-    	async.httpGet(self.port, string.format("/log?packet=%s", table.concat(self._data, "|")))
-		self._data = {}
+		if #self.data == 0 then return end
+    	async.httpGet(self.port, string.format("/log?packet=%s", table.concat(self.data, "|")))
+		self.data = {}
 	end
 	return this
 end
