5df54e5613687cd32763fd3a0c6850863dd1a7a3
Chris Wright <chris@inkyspider.co.uk>
2020-11-27 15:16:25 +0000

    Add initialiser method to FIFO

--- util/fifo/src.lua
+++ util/fifo/src.lua
@@ -7,6 +7,9 @@ function FIFO(size)
 		self.i = self.i%self.size +1
 		self.data[self.i] = val
 	end
+	function this:set_all(val)
+		for i=1,self.size do self:push(val) end
+	end
 	function this:get(i)
 		if i < 0 then return self:get(self.size+i+1) end
 		return self.data[(self.i+i-1)%self.size +1]
