1c6864ad5463cf462c691283106d04a532f9c5f4
Chris Wright <chris@inkyspider.co.uk>
2021-09-04 15:52:43 +0100

    Remove private underscore from control input objects

--- util/control_input/src.lua
+++ util/control_input/src.lua
@@ -1,10 +1,10 @@
 function CtlInput(chan,sensitivity)
-	local t = {_raw=0,_v=0}
-	function t:tick() self._raw=input.getNumber(chan) end
+	local t = {raw=0,_v=0}
+	function t:tick() self.raw=input.getNumber(chan) end
 	function t:smooth()
-		self._v=self._v+(self._raw-self._v)*(sensitivity/100)^2
+		self._v=self._v+(self.raw-self._v)*(sensitivity/100)^2
 		return self._v
 	end
-	function t:is_active() return math.abs(self._raw) > 0.9 end
+	function t:is_active() return math.abs(self.raw) > 0.9 end
 	return t
 end
